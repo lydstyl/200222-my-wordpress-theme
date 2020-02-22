2be3ef57a21e557c9a0038783984761d1fe6e1de
Gabriel Brun <lydstyl@gmail.com>
2020-02-22 20:40:42 +0100

    pagination easy

--- wp-content/themes/myTheme/archive.php
+++ wp-content/themes/myTheme/archive.php
@@ -3,6 +3,9 @@
 <section class="page-wrap">
   <div class="container">
     <?php get_template_part( 'includes/section', 'archive' ) ?>
+
+    <?php previous_posts_link( ); ?>
+    <?php next_posts_link( ); ?>
   </div>
 </section>
 <?php get_footer( ); ?>
