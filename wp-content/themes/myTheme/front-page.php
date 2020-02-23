d5b2609f44e9618e5c3b32471f7d04bc3871f882
Gabriel Brun <lydstyl@gmail.com>
2020-02-23 17:36:53 +0100

    search

--- wp-content/themes/myTheme/front-page.php
+++ wp-content/themes/myTheme/front-page.php
@@ -6,6 +6,9 @@
 
     <?php get_template_part( 'includes/section', 'content' ) ?>
 
+    <?= get_search_form() ?>
+
+
     <a href="https://www.youtube.com/watch?v=n3EcEYFgyrQ&list=PLgFB6lmeXFOpHnNmQ4fdIYA5X_9XhjJ9d">WordPress Theme Development From Scratch</a>
   </div>
 </section>
